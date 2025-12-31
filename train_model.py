6609c498e13385a5ae5b8028921380d018fdd3fc
priyanshu494 <67260333+priyanshu494@users.noreply.github.com>
2025-12-31 16:48:16 +0530

    Reorganize imports and improve model training

--- train_model.py
+++ train_model.py
@@ -35,7 +35,7 @@ X_test = test_data.drop('target', axis=1).values.astype('float32')
 y_test = test_data.loc[:, 'target'].values.astype('int32')
 
 # Logistic Regression
-logreg = LogisticRegression(C=0.0001, solver='lbfgs', max_iter=100, multi_class='multinomial')
+logreg = LogisticRegression(C=0.0001, solver='lbfgs', max_iter=100)
 logreg.fit(X_train, y_train)
 predictions_lr = logreg.predict(X_test)
 
@@ -135,5 +135,6 @@ with open('scores.txt', "w") as score:
     score.write("F1 Score: %2.1f%%\n" % f1_lr)
     score.write("Recall Score: %2.1f%%\n" % recall_lr)
     score.write("Precision Score: %2.1f%%\n" % prec_lr)
+
 
 
